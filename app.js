cd72b6ead1e5637f5e07f014532c085e76fb26ab
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-02-09 19:40:00 -0800

    scroll with jquery complete

--- app.js
+++ app.js
@@ -2,30 +2,41 @@
 
 
 $(()=>{
-    const menu = $('.menu');
-    const menuItem = $('.menuItem');
-    const hamburger = $('.hamburger');
-    const closeIcon = $('.closeIcon');
-    const menuIcon = $('.menuIcon')
-
+    //hamburger menu
     const toggleMenu = () => {
-        if(menu.hasClass("showMenu")){
-            menu.removeClass("showMenu");
-            closeIcon.css('display', 'none')
-            menuIcon.css('display', 'block')
+        if($('.menu').hasClass("showMenu")){
+            $('.menu').removeClass("showMenu");
+            $('.closeIcon').css('display', 'none')
+            $('.menuIcon').css('display', 'block')
 
         }
         else{
-            menu.addClass("showMenu");
-            closeIcon.css('display', 'block')
-            menuIcon.css('display', 'none')
+            $('.menu').addClass("showMenu");
+            $('.closeIcon').css('display', 'block')
+            $('.menuIcon').css('display', 'none')
         }
     }
 
-    hamburger.on('click', toggleMenu);
+    $('.hamburger').on('click', toggleMenu);
     //closes window when clicked
-    for(let i = 0; i < menuItem.length; i++){
-        menuItem.on('click', toggleMenu);
+    for(let i = 0; i < $('.menuItem').length; i++){
+        $('.menuItem').on('click', toggleMenu);
     }
 
-})
+
+    // smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+            return false;
+            }
+        }
+    });
+
+
+    })
